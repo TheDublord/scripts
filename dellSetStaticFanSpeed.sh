e51b3655cc3e92c3595b1d57b771e7b18ab14699
TheDublord <25010475+TheDublord@users.noreply.github.com>
2025-03-19 10:41:39 -0700

    Update dellSetStaticFanSpeed.sh
    removed wait times cuz not needed

--- dellSetStaticFanSpeed.sh
+++ dellSetStaticFanSpeed.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-
 read -p "Enter the IP of iDRAC: " IDRAC_IP
 read -p "Enter the iDRAC username: " IDRAC_USER
 read -p "Enter the iDRAC password: " IDRAC_PASSWORD
-echo "hacking server in background"
+# To set static entry: Connect read statement, uncomment below variables
+#IDRAC_IP="IP"
+#IDRAC_USER="user"
+#IDRAC_PASSWORD="pass"
+echo "hacking server beep boop beep boop"
 clear
 read -p "Enter fan speed as a % (NOTE: 0-100 is valid, but >50% might be excessive): " FAN_SPEED
+# Converts entered number into hex, and appends 0x to make ipmitool happy
 hex_speed=$(printf "0x%x" $FAN_SPEED)
 echo "Enabling manual mode..."
 ipmitool -I lanplus -H $IDRAC_IP -U $IDRAC_USER -P $IDRAC_PASSWORD raw 0x30 0x30 0x01 0x00
-echo "Manual mode enabled!"
-sleep 1
 echo "Setting fans to desired speed..."
 ipmitool -I lanplus -H $IDRAC_IP -U $IDRAC_USER -P $IDRAC_PASSWORD raw 0x30 0x30 0x02 0xff $hex_speed
-sleep 2
-echo "Speed set sucessfully, exiting...."
-sleep 1
-exit 0
+
